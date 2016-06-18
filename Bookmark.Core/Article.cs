7ca5b611a999147d5e211d5bf3c5d605c7f2be01
randelramirez <randelramirez1@gmail.com>
2016-06-18 23:49:53 +0800

    Add project files.

---
+++ Bookmark.Core/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmark.Core
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string URL { get; set; }
+    }
+}
