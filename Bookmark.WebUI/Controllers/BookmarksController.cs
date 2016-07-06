a8cbffdce57154d924919e4a73da2dfedc2632b9
randelramirez <randelramirez1@gmail.com>
2016-07-07 00:19:05 +0800

    Created Controller and added views for add and delete. See backlogs at Visual Studio Team Services for TODOs.

---
+++ Bookmark.WebUI/Controllers/BookmarksController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bookmark.Infrastructure;
+using System.Net;
+
+namespace Bookmark.WebUI.Controllers
+{
+    public class BookmarksController : Controller
+    {
+        private BookmarkContext dataContext = new Infrastructure.BookmarkContext();
+
+        // GET: Bookmarks
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Core.Bookmark bookmark)
+        {
+            if (ModelState.IsValid)
+            {
+                this.dataContext.Bookmarks.Add(bookmark);
+                this.dataContext.SaveChanges();
+                return Redirect("Index");
+            }
+            return View(bookmark);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var bookmark = this.dataContext.Bookmarks.Find(id);
+            if (bookmark == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookmark);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var bookmark = this.dataContext.Bookmarks.Find(id);
+            if (bookmark == null)
+            {
+                return HttpNotFound();
+            }
+            this.dataContext.Bookmarks.Remove(bookmark);
+            this.dataContext.SaveChanges();
+            return Redirect("Index");
+        }
+    }
+}
